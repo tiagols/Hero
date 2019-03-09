e76a5c320b15b7090194d3a77f47728137e1b3d9
Tiago Santos <tiago@morriscs.com>
2019-03-09 12:09:24 -0500

    http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule }    from '@angular/common/http';
 
 @NgModule({
   declarations: [
